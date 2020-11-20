120cdb0ed9adda64c8152e1160d40777f747d40c
40418893 <40418893@live.napier.ac.uk>
2020-11-20 14:36:46 +0630

    Use Case issus added and error solve on integration testing and unit testing

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -12,7 +12,14 @@ public class App
         App a = new App();
 
         // Connect to database
-        a.connect("localhost:33060");
+        if (args.length < 1)
+        {
+            a.connect("localhost:33060");
+        }
+        else
+        {
+            a.connect(args[0]);
+        }
 
         // Extract world information
         ArrayList<country> countrylist = a.getCountryList();
@@ -107,14 +114,14 @@ public class App
 
     /**
      * Connect to the MySQL database.
-     * @param s
+     * @param
      */
-    public void connect(String s)
+    public void connect(String location)
     {
         try
         {
             // Load Database driver
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         }
         catch (ClassNotFoundException e)
         {
@@ -129,9 +136,9 @@ public class App
             try
             {
                 // Wait a bit for db to start
-                Thread.sleep(3000);
+                Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://" + location + "/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             }
---
+++ use-case/Use_Case16.md
@@ -0,0 +1,29 @@
+#CHARACTERISTIC INFORMATION
+#Goal in Context
+
+16. As a client I want to check the most popular cities so that I can view the top 10 populated cities in a district.  
+
+##Scope
+Student Group Project
+##Level
+Primary Task 
+##Preconditions
+We know the role. Database contains current World data (World.sql).
+##Success End Condition
+Will show the top populated countries in a continent
+##Failed End Condition
+Output will not produce 
+##Primary Actor
+Client 
+##Trigger
+ Cleint collects the World data from world.sql 
+##MAIN SUCCESS SCENARIO
+ 1. Client collects name of the countries and population to get the top populated countries for.
+ 2. Filters the top populated countries 
+ 3. Print the top populated countries   
+##EXTENSIONS
+Clinet does not exit
+##SUB-VARIATIONS
+None 
+##SCHEDULE
+DueDate: Releases 1.0 
